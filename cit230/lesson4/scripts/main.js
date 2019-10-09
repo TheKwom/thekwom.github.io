c541e0c0653283d5fede644abe2130dd343a4ecc
TheKwom <dafwendz@gmail.com>
2019-10-09 11:12:17 -0600

    date

---
+++ cit230/lesson4/scripts/main.js
@@ -0,0 +1,44 @@
+var currentDate = new Date();
+var currentDateString;
+
+// get day of week
+var weekDayNumber = currentDate.getDay();
+
+var daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+var weekDay = daysOfWeek[weekDayNumber];
+
+// get month
+var months = [
+    'January',
+    'February',
+    'March',
+    'Arpil',
+    'May',
+    'June',
+    'July',
+    'August',
+    'Spetember',
+    'OCtober',
+    'Novemeber',
+    'December'
+];
+
+var monthNumber = currentDate.getMonth();
+
+var month = months[monthNumber];
+
+
+// outoput
+currentDateString = weekDay + ', ' + currentDate.getDate() + ' ' + month + ' ' + currentDate.getFullYear;
+
+// DEBUG;
+console.log(currentDateString);

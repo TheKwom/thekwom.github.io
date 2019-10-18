6170b753a37df9262a2cba2e1ced314bcbf84793
TheKwom <dafwendz@gmail.com>
2019-10-18 10:57:17 -0600

    colorchanges + forecast

--- cit230/lesson5/scripts/main.js
+++ cit230/lesson5/scripts/main.js
@@ -46,5 +46,25 @@ document.getElementById('currentDate').innerHTML = currentDateString;
 console.log(currentDateString);
 
 function toggleMenu() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+// create five day forecast
+let currentDay = weekDayNumber;
+
+for (let i = 1; i < 6; i++) {
+    // add one day to current day
+    currentDay++;
+
+    // if current day is greater than 6 (Saturday), reset it to 0 (Sunday)
+    if (currentDay > 6) {
+        currentDay = 0;
+    }
+
+    // assign value to placeholder in HTML
+    const element = document.getElementById(`day${i}`);
+    // const element = document.getElementById('day' + i);
+
+    element.innerHTML = daysOfWeek[currentDay];
 }

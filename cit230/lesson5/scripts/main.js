e4dd9f92d513f6c75bd21294d1f379c187b1a3a0
TheKwom <dafwendz@gmail.com>
2019-10-18 11:15:17 -0600

    commitNewChanges

--- cit230/lesson5/scripts/main.js
+++ cit230/lesson5/scripts/main.js
@@ -68,3 +68,7 @@ for (let i = 1; i < 6; i++) {
 
     element.innerHTML = daysOfWeek[currentDay];
 }
+
+if(weekDayNumber === 5) {
+    document.getElementById("pancake").removeAttribute("class", "hidden");
+}
